9ef143439dce27a29f2dc629cf291fc89801e8f4
RyanBluth <ryanbluth93@gmail.com>
2017-12-30 09:54:52 -0500

    Refactoring and travis

--- src/table/mod.rs
+++ src/table/mod.rs
@@ -23,46 +23,32 @@ impl<'data> Table<'data> {
         self.rows.push(row);
     }
 
-    pub fn print(&mut self) {
-        let mut print_buffer = String::new();
-        let max_widths = self.calculate_max_column_widths();
-        let total_width = max_widths.iter().sum::<usize>() + 4;
-        let separator = Table::gen_separator(&max_widths);
-        Table::buffer_line(&mut print_buffer, &separator);
-        for row in &self.rows {
-            Table::buffer_line(&mut print_buffer, &self.format_row(&row, &max_widths));
-            Table::buffer_line(&mut print_buffer, &separator);
-        }
-        //Table::buffer_line(&mut print_buffer, &separator);
-        println!("{}", print_buffer);
-    }
-
     pub fn format_row(&self, row: &Row<'data>, max_widths: &Vec<usize>) -> String {
         let mut buf = String::new();
         let mut span_count = 1;
         let mut col_idx = 0;
         for en in max_widths.into_iter().enumerate() {
-            if row.columns.len() > col_idx {
+            if row.cells.len() > col_idx {
                 if span_count == 1 {
                     let mut pad_len = 0;
-                    if *en.1 > row.columns[col_idx].width(){
-                        pad_len = en.1 - row.columns[col_idx].width();
+                    if *en.1 > row.cells[col_idx].width(){
+                        pad_len = en.1 - row.cells[col_idx].width();
                     }
                     if 0 == 1 {
                         let pad_front_len = f32::ceil(pad_len as f32 / 2f32) as usize;
                         let pad_front = str::repeat(" ", pad_front_len);
                         let pad_end_len = pad_len - pad_front_len;
                         let pad_end = str::repeat(" ", pad_end_len);
-                        buf.push_str(format!("|{}{}{}", pad_front, row.columns[col_idx], pad_end)
+                        buf.push_str(format!("|{}{}{}", pad_front, row.cells[col_idx], pad_end)
                             .as_str());
                     } else {
-                        buf.push_str(format!("|{}{}", row.columns[col_idx], str::repeat(" ", pad_len))
+                        buf.push_str(format!("|{}{}", row.cells[col_idx], str::repeat(" ", pad_len))
                             .as_str());
                     }
                 }else{
                     buf.push_str(format!("{} ", str::repeat(" ", *en.1)).as_str());
                 }
-                if span_count < row.columns[col_idx].col_span {
+                if span_count < row.cells[col_idx].col_span {
                     span_count += 1;
                 }else{
                     span_count = 1;
@@ -76,27 +62,30 @@ impl<'data> Table<'data> {
         return buf;
     }
 
-    pub fn gen_separator(max_widths: &Vec<usize>) -> String {
-        let mut buf = String::new();
-        buf.push('+');
-        for width in max_widths {
-            if buf.len() > 1 {
-                buf.push('+');
+    pub fn print(&mut self) {
+        let mut print_buffer = String::new();
+        let max_widths = self.calculate_max_column_widths();
+        let total_width = max_widths.iter().sum::<usize>() + 4;
+        if self.rows.len() > 0 {
+            let mut separator = String::new();
+            for row in &self.rows {
+                separator = row.get_separator(&max_widths);
+                Table::buffer_line(&mut print_buffer, &separator);
+                Table::buffer_line(&mut print_buffer, &self.format_row(&row, &max_widths));
             }
-            buf.push_str(str::repeat("-", *width).as_str());
+            Table::buffer_line(&mut print_buffer, &separator);
+            println!("{}", print_buffer);
         }
-        buf.push('+');
-        return buf;
     }
 
     pub fn calculate_max_column_widths(&self) -> Vec<usize> {
         let mut max_widths: Vec<usize> = Vec::new();
         for row in &self.rows {
-            for i in 0..row.columns.len() {
+            for i in 0..row.cells.len() {
                 if max_widths.len() <= i {
-                    max_widths.push(row.columns[i].width());
+                    max_widths.push(row.cells[i].width());
                 } else {
-                    max_widths[i] = max(max_widths[i], row.columns[i].width());
+                    max_widths[i] = max(max_widths[i], row.cells[i].width());
                 }
             }
         }
--- src/table/row.rs
+++ src/table/row.rs
@@ -1,17 +1,17 @@
 use table::cell::Cell;
 
 pub struct Row<'data> {
-    pub columns: Vec<Cell<'data>>,
+    pub cells: Vec<Cell<'data>>,
 }
 
 impl<'data> Row<'data> {
     pub fn new<T>(cells: Vec<T>) -> Row<'data>
         where T: Into<Cell<'data>>
     {
-        let mut row = Row { columns: vec![] };
+        let mut row = Row { cells: vec![] };
 
         for entry in cells {
-            row.columns.push(entry.into());
+            row.cells.push(entry.into());
         }
 
         return row;
@@ -19,9 +19,26 @@ impl<'data> Row<'data> {
 
     pub fn column_widths(&self) -> Vec<usize> {
         let mut widths = Vec::new();
-        for column in &self.columns {
+        for column in &self.cells {
             widths.push(column.width());
         }
         return widths;
+    }
+
+    pub fn get_separator(&self, max_widths: &Vec<usize>) -> String {
+        let mut buf = String::new();
+        let mut span_count = 0;
+        buf.push('+');
+        for i in 0..max_widths.len() {
+            if buf.len() > 1 && span_count == self.cells[i - span_count].col_span{
+                buf.push('+');
+            }else if span_count > 0{
+                buf.push('-');
+            }
+            buf.push_str(str::repeat("-", max_widths[i]).as_str());
+            span_count += 1;
+        }
+        buf.push('+');
+        return buf;
     }
 }
